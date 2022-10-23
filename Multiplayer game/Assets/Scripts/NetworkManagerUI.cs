57ec1cb63ccdc74e9209b9fe0fa0b1b25d78526a
Elio Bouwhuis <50573900+Elio-Bouwhuis@users.noreply.github.com>
2022-10-23 09:55:17 +0200

    patch 010
    Added game timer and start button

--- Multiplayer game/Assets/Scripts/NetworkManagerUI.cs
+++ Multiplayer game/Assets/Scripts/NetworkManagerUI.cs
@@ -10,13 +10,22 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private Button startBtnBtn;
     [SerializeField] private Image backgroundImage;
     [SerializeField] private TextMeshProUGUI scorePlayerOne;
     [SerializeField] private TextMeshProUGUI scorePlayerTwo;
     [SerializeField] private GameObject inputField;
+    [SerializeField] private GameObject startBtn;
+
+    [SerializeField] TextMeshProUGUI gameTimer;
+
+    private int time = 300;
+    private float timePassed = 0f;
 
     private void Awake()
     {
+        time = 300;
+        timePassed = 0f;
         serverBtn.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartServer();
@@ -27,6 +36,7 @@ public class NetworkManagerUI : MonoBehaviour
             Destroy(inputField.gameObject);
             scorePlayerOne.enabled = true;
             scorePlayerTwo.enabled = true;
+            startBtn.SetActive(true);
         });
         hostBtn.onClick.AddListener(() =>
         {
@@ -38,6 +48,7 @@ public class NetworkManagerUI : MonoBehaviour
             Destroy(inputField.gameObject);
             scorePlayerOne.enabled = true;
             scorePlayerTwo.enabled = true;
+            startBtn.SetActive(true);
         });
         clientBtn.onClick.AddListener(() =>
         {
@@ -49,6 +60,30 @@ public class NetworkManagerUI : MonoBehaviour
             Destroy(inputField.gameObject);
             scorePlayerOne.enabled = true;
             scorePlayerTwo.enabled = true;
+            startBtn.SetActive(true);
         });
+        startBtnBtn.onClick.AddListener(() =>
+        {
+            Debug.Log("start button clicked");
+            gameTimer.enabled = true;
+            //gameTimer = GameObject.Find("TimeLeft").GetComponent<TextMeshProUGUI>();
+            Destroy(startBtn.gameObject);
+        });
+    }
+
+    private void Update()
+    {
+        timePassed += Time.deltaTime;
+        if (timePassed > 1.0f)
+        {
+            time -= 1;
+            timePassed = 0f;
+            Debug.Log(time);
+            //gameTimer.text = "Time left: " + i;
+            if(time == 0)
+            {
+                Debug.Log("Game Over!");
+            }
+        }
     }
 }
