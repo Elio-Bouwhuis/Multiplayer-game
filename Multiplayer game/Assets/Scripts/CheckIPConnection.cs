2b82d2b8f3ce729656e5e4a6b47ba41595b962b2
Elio Bouwhuis <50573900+Elio-Bouwhuis@users.noreply.github.com>
2022-11-08 12:51:11 +0100

    Patch 012B
    changes to connection

--- Multiplayer game/Assets/Scripts/CheckIPConnection.cs
+++ Multiplayer game/Assets/Scripts/CheckIPConnection.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Unity.Netcode;
 
-public class CheckIPConnection : MonoBehaviour
+public class CheckIPConnection : NetworkBehaviour
 {
 
     [SerializeField] Unity.Netcode.Transports.UTP.UnityTransport unityTransport;
@@ -11,6 +12,11 @@ public class CheckIPConnection : MonoBehaviour
 
     public void UpdateAdress()
     {
-        unityTransport.ConnectionData.Address = inputField.text;
+        //unityTransport.ConnectionData.Address = inputField.text;
+        NetworkManager.Singleton.GetComponent<Unity.Netcode.Transports.UTP.UnityTransport>().SetConnectionData(
+            inputField.text,  // The IP address is a string
+            7777, // The port number is an unsigned short (ushort)12345
+            "0.0.0.0" // The server listen address is a string.
+        );
     }
 }
